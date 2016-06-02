f765aa6dafc59140e3b90fc17702a87229279d47
Oren Levitzky <oren.levitzky@crossrider.com>
2016-06-02 17:42:33 +0300

    botkit style

--- api/ebay.js
+++ api/ebay.js
@@ -21,51 +21,50 @@ class EbayApi {
       return instance;
     }
 
-    search(keywords, options) {
+    search(keywords) {
         console.log("Ebay search...");
-        ebay.xmlRequest({
-                serviceName: 'Finding',
-                opType: 'findItemsByKeywords',
-                appId: API_KEY,
-                params: Object.assign(this.defaultParams, {keywords: keywords}),
-                parser: ebay.parseResponseJson
-            },
-            function itemsCallback(error, itemsResponse) {
-                var resItems = [];
-                var totalEntries = 0;
-                if (error) {
-                    console.log("Sorry, something went wrong:", error);
-                } else {
-                    if (itemsResponse.searchResult.$.count > 0) {
-                      totalEntries = itemsResponse.paginationOutput.totalEntries;
-                      var items = itemsResponse.searchResult.item;
-                      console.log(`***** Found ${totalEntries} items *****\n\n`);
-                      for (var i = 0; i < items.length; i++) {
-                        var item = items[i];
-                        resItems.push({
-                            title: item.title,
-                            subtitle: `${item.sellingStatus.currentPrice.amount} ${item.sellingStatus.currentPrice.currencyId}`,
-                            image_url: item.galleryURL,
-                            buttons: [
-                                {
-                                    type: 'web_url',
-                                    url: item.viewItemURL,
-                                    title: 'View item'
-                                }
-                            ]
-                        })
-                      }  
+        return new Promise(function(resolve, defered) {
+            ebay.xmlRequest({
+                    serviceName: 'Finding',
+                    opType: 'findItemsByKeywords',
+                    appId: API_KEY,
+                    params: Object.assign(this.defaultParams, {keywords: keywords}),
+                    parser: ebay.parseResponseJson
+                },
+                function itemsCallback(error, itemsResponse) {
+                    var resItems = [];
+                    var totalEntries = 0;
+                    if (error) {
+                        console.log("Sorry, something went wrong:", error);
                     } else {
-                      console.log("There are no results for your search.");
+                        if (itemsResponse.searchResult.$.count > 0) {
+                          totalEntries = itemsResponse.paginationOutput.totalEntries;
+                          var items = itemsResponse.searchResult.item;
+                          console.log(`***** Found ${totalEntries} items *****\n\n`);
+                          for (var i = 0; i < items.length; i++) {
+                            var item = items[i];
+                            resItems.push({
+                                title: item.title,
+                                subtitle: `${item.sellingStatus.currentPrice.amount} ${item.sellingStatus.currentPrice.currencyId}`,
+                                image_url: item.galleryURL,
+                                buttons: [
+                                    {
+                                        type: 'web_url',
+                                        url: item.viewItemURL,
+                                        title: 'View item'
+                                    }
+                                ]
+                            })
+                          }  
+                        } else {
+                          console.log("There are no results for your search.");
+                        }
                     }
+                    
+                    resolve({ items: resItems, totalEntries: totalEntries });
                 }
-
-                options.callback(
-                    { items: resItems, totalEntries: totalEntries },
-                    options.sender
-                );
-            }
-        );
+            );
+        }.bind(this));
     }
 
 }
--- server.js
+++ server.js
@@ -1,19 +1,87 @@
-var express = require('express');
-var app = express();                 // define our app using express
-var bodyParser = require('body-parser');
-var routes = require('./routes');
+var EbayApi = require('./api/ebay');
+var searchHandler = require('./search_handler');
+var Botkit = require('botkit');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+var controller = Botkit.facebookbot({
+    debug: true,
+    access_token: "EAAKZCjmh9l8IBAInBtUODtcMw9ZCACjFUpvsKWUfWhaWe0drqeY5P8RZBQdohlJGDqthCZCQioglyjm88waJgJ4kdNTcDgUYAcVHEMfsuHZC6i9HBy2dddTlZBxe3nq7TRCapZCrZCHQtMoRgbsfH1YseZA2PvGBZBzZADdwT8KQSnbewZDZD",
+    verify_token: "orenCross",
+});
 
-var port = process.env.PORT || 8099;
+var bot = controller.spawn({
+});
 
-app.use('/', routes);
-app.use(express.static(__dirname + "/views" ));
+controller.setupWebserver('8099',function(err,webserver) {
+  controller.createWebhookEndpoints(controller.webserver, bot, function() {
+      console.log('This bot is online!!!');
+  });
+});
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+// this is triggered when a user clicks the send-to-messenger plugin
+controller.on('message_received', function(bot, message) {
+    console.log("Incoming message:");
+});
+
+// user said hello
+controller.hears(['Eran!'], 'message_received', function(bot, message) {
+    bot.reply(message, 'Zehavi');
+    bot.reply(message, {
+      attachment: {
+        type: 'image',
+        payload: {
+          url: "http://sport1.maariv.co.il/is/772x434/dannymaron208.jpg"
+        }
+      }
+    });
+});
+
+controller.hears(['(.*)help me(.*)', 'i dont know', '(.*)help(.*)'], 'message_received', function(bot, message) {
+    askWhatToBuy = function(response, convo) {
+      convo.ask('Sure. what is your gender?', [
+        {
+          pattern: '^male',
+          callback: function(response, convo) {
+            convo.say('Sir.');
+            convo.say("I will gamble you like sports. Here are some sports items:");
+            EbayApi.search(['sports shoes']).then((result) => {
+                convo.say({
+                  attachment: {
+                    type: 'template',
+                    payload: {
+                      template_type: 'generic',
+                      elements: result.items
+                    }
+                  }
+                })
+                convo.next();
+            }).catch((error) => {
+                console.log("ERORRRRRRRRRRRR", error);
+            });
+          }
+        },
+        {
+          pattern: '^female',
+          callback: function(response, convo) {
+            convo.say('Me lady.');
+            convo.say("Shoes are always good. Here are some interesting items:");
+            EbayApi.search(['lady shoes']).then((result) => {
+                convo.say({
+                  attachment: {
+                    type: 'template',
+                    payload: {
+                      template_type: 'generic',
+                      elements: result.items
+                    }
+                  }
+                })
+                convo.next();
+            }).catch((error) => {
+                console.log("ERORRRRRRRRRRRR", error);
+            });
+          }
+        }
+      ])  
+    }
+    
+    bot.startConversation(message, askWhatToBuy);
+});
